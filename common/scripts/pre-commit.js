7e48ad52b185b8131e9b8940b59f6be55f3608c5
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-03-01 12:30:15 -0500

    Update common/scripts/pre-commit.js
    Co-authored-by: Raphaël LEMIEUX <1904889+raplemie@users.noreply.github.com>

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -4,10 +4,10 @@ async function preCommit() {
   const success = await lintStaged({
     config: {
       "*.{ts,tsx}": [
-        "node ./common/scripts/copyright-linter.js --",
         "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --fix",
         "node ./common/scripts/node_modules/prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
         "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.json --ignore-path ./.eslintignore --color",
+        "node ./common/scripts/copyright-linter.js --",
       ],
       "*.{md,json}": [
         "node ./common/scripts/node_modules/prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
