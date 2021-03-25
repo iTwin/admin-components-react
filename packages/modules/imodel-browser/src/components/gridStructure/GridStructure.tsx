7f2439f1262237f1f72152af032055783ac0a6c4
raplemie <1904889+raplemie@users.noreply.github.com>
2021-03-25 17:04:54 -0400

    Stop using css modules

--- packages/modules/imodel-browser/src/components/gridStructure/GridStructure.tsx
+++ packages/modules/imodel-browser/src/components/gridStructure/GridStructure.tsx
@@ -2,12 +2,17 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+
+import "./GridStructure.scss";
+
 import classNames from "classnames";
 import React from "react";
 
-import styles from "./GridStructure.module.scss";
 export const GridStructure = (props: React.HTMLProps<HTMLDivElement>) => {
   return (
-    <div {...props} className={classNames(styles.base, props.className)} />
+    <div
+      {...props}
+      className={classNames("iac-grid-structure", props.className)}
+    />
   );
 };
