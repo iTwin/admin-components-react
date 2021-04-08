873a9c2cc24d8ddb10b24071036f4a05b0d96bf5
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-04-08 14:11:04 -0400

    add comment to test publish; update workflows

--- packages/modules/imodel-browser/src/components/gridStructure/GridStructure.tsx
+++ packages/modules/imodel-browser/src/components/gridStructure/GridStructure.tsx
@@ -1,13 +1,14 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 
 import "./GridStructure.scss";
 
 import classNames from "classnames";
 import React from "react";
 
+/** Browser container structure */
 export const GridStructure = (props: React.HTMLProps<HTMLDivElement>) => {
   return (
     <div
