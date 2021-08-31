e14bfff89a21dd1c8c0235dd2934fa5402c3a73f
Vilius <36186912+bentleyvk@users.noreply.github.com>
2021-08-31 18:25:17 +0300

    Moved `ManageVersionsTabs` to regular exports so it could be used when setting `currentTab`. (#49)

--- packages/modules/manage-versions/src/index.ts
+++ packages/modules/manage-versions/src/index.ts
@@ -2,11 +2,11 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export { ManageVersions } from "./components/ManageVersions/ManageVersions";
-export type {
-  ManageVersionsProps,
+export {
+  ManageVersions,
   ManageVersionsTabs,
 } from "./components/ManageVersions/ManageVersions";
+export type { ManageVersionsProps } from "./components/ManageVersions/ManageVersions";
 
 export type {
   ApiOverrides,
