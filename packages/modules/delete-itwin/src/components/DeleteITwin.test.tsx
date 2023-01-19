1cbb61e09aca0e2b3d004635cabe3a53f3015ad2
will <38542573+williamkbentley@users.noreply.github.com>
2023-01-19 10:22:59 -0500

    Added delete iTwin component (#64)

---
+++ packages/modules/delete-itwin/src/components/DeleteITwin.test.tsx
@@ -0,0 +1,100 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { act, render } from "@testing-library/react";
+import React from "react";
+
+import { DeleteITwin } from "./DeleteITwin";
+
+describe("DeleteITwin", () => {
+  const fetchMock = jest.fn(() => Promise.resolve({ ok: true } as Response));
+  global.fetch = fetchMock;
+
+  beforeEach(() => {
+    fetchMock.mockClear();
+  });
+
+  it("should open delete iTwin", () => {
+    const { getByText } = render(
+      <DeleteITwin
+        iTwin={{ id: "111", displayName: "test iTwin" }}
+        accessToken="dd"
+      />
+    );
+
+    const title = document.querySelector(
+      ".iac-delete-itwin-dialog-title"
+    ) as HTMLElement;
+    expect(title.textContent).toBe(`Delete an iTwin 'test iTwin'`);
+    expect(title.querySelector(".iac-warning-icon")).toBeTruthy();
+    getByText(
+      "Deleting this iTwin will remove access for all users and all data will no longer be available. Are you sure you want to delete this iTwin?"
+    );
+  });
+
+  it("should delete iTwin", async () => {
+    const successMock = jest.fn();
+
+    const { getByText } = render(
+      <DeleteITwin
+        iTwin={{ id: "111", displayName: "test iTwin" }}
+        apiOverrides={{ serverEnvironmentPrefix: "dev" }}
+        accessToken="dd"
+        onSuccess={successMock}
+      />
+    );
+
+    const button = getByText("Yes") as HTMLButtonElement;
+    await act(async () => button.click());
+    expect(fetchMock).toHaveBeenCalledWith(
+      "https://dev-api.bentley.com/itwins/111",
+      {
+        method: "DELETE",
+        headers: { Authorization: "dd" },
+      }
+    );
+    expect(successMock).toHaveBeenCalled();
+  });
+
+  it("should return error when failed to delete iTwin", async () => {
+    const failureMock = jest.fn();
+    const error = new Error("Fail");
+    fetchMock.mockImplementationOnce(() => Promise.reject(error));
+    const { getByText } = render(
+      <DeleteITwin
+        iTwin={{ id: "111", displayName: "test iTwin" }}
+        accessToken="dd"
+        onError={failureMock}
+      />
+    );
+
+    const button = getByText("Yes") as HTMLButtonElement;
+    await act(async () => button.click());
+    expect(fetchMock).toHaveBeenCalledWith(
+      "https://api.bentley.com/itwins/111",
+      {
+        method: "DELETE",
+        headers: { Authorization: "dd" },
+      }
+    );
+    expect(failureMock).toHaveBeenCalledWith(error);
+  });
+
+  it("should close dialog", () => {
+    const closeMock = jest.fn();
+
+    const { getByText } = render(
+      <DeleteITwin
+        iTwin={{ id: "111", displayName: "test iTwin" }}
+        accessToken="dd"
+        onClose={closeMock}
+      />
+    );
+
+    const button = getByText("No") as HTMLButtonElement;
+    act(() => button.click());
+    expect(fetchMock).not.toHaveBeenCalled();
+    expect(closeMock).toHaveBeenCalled();
+  });
+});
---
+++ packages/modules/delete-itwin/src/components/DeleteITwin.tsx
@@ -0,0 +1,138 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import "./DeleteITwin.scss";
+
+import SvgWarning from "@itwin/itwinui-icons-react/cjs/icons/StatusWarning";
+import {
+  Button,
+  Modal,
+  ModalButtonBar,
+  ProgressRadial,
+  toaster,
+} from "@itwin/itwinui-react";
+import React from "react";
+
+export type DeleteITwinProps = {
+  /** Name and Id of iTwin object to delete */
+  iTwin: { displayName?: string; id: string };
+  /** Bearer access token with scope `itwins:modify`. */
+  accessToken: string;
+  /** Object that configures different overrides for the API. */
+  apiOverrides?: { serverEnvironmentPrefix?: "dev" | "qa" | "" };
+  /** Callback on closed dialog. */
+  onClose?: () => void;
+  /** Callback on failed delete. */
+  onError?: (error: Error) => void;
+  /** Callback on successful delete. */
+  onSuccess?: () => void;
+  /** Object of string overrides. */
+  stringsOverrides?: {
+    /** Displayed after successful delete. */
+    successMessage?: string;
+    /** Displayed after failed delete. */
+    errorMessage?: string;
+    /** The title of the dialog. */
+    title?: string;
+    /** Main body message. */
+    bodyMessage?: string;
+    /** Confirm button string. */
+    confirmButton?: string;
+    /** Cancel button string. */
+    cancelButton?: string;
+  };
+};
+
+export const DeleteITwin = (props: DeleteITwinProps) => {
+  const {
+    iTwin,
+    accessToken,
+    apiOverrides = { serverEnvironmentPrefix: "" },
+    onClose,
+    onError,
+    onSuccess,
+    stringsOverrides,
+  } = props;
+  const [isOpen, setIsOpen] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const onDelete = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://${
+          apiOverrides?.serverEnvironmentPrefix &&
+          `${apiOverrides.serverEnvironmentPrefix}-`
+        }api.bentley.com/itwins/${iTwin.id}`,
+        { method: "DELETE", headers: { Authorization: `${accessToken}` } }
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      } else {
+        setIsLoading(false);
+        toaster.positive(
+          stringsOverrides?.successMessage ?? "iTwin deleted successfully."
+        );
+        onSuccess?.();
+      }
+    } catch (err: any) {
+      error(err);
+    } finally {
+      close();
+    }
+  };
+
+  const error = (error: Error) => {
+    setIsLoading(false);
+    toaster.negative(
+      stringsOverrides?.errorMessage ??
+        "Could not delete an iTwin. Please try again later."
+    );
+    onError?.(error);
+  };
+
+  const close = React.useCallback(() => {
+    setIsOpen(false);
+    onClose?.();
+  }, [onClose]);
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        style={{ maxWidth: 600 }}
+        onClose={close}
+        title={
+          <div className="iac-delete-itwin-dialog-title">
+            <SvgWarning className="iac-warning-icon" />
+            {stringsOverrides?.title ??
+              `Delete an iTwin ${
+                iTwin.displayName && `'${iTwin.displayName}'`
+              }`}
+          </div>
+        }
+      >
+        {stringsOverrides?.bodyMessage ??
+          "Deleting this iTwin will remove access for all users and all data will no longer be available. Are you sure you want to delete this iTwin?"}
+        <ModalButtonBar>
+          <Button styleType="high-visibility" onClick={onDelete}>
+            {stringsOverrides?.confirmButton ?? "Yes"}
+          </Button>
+          <Button onClick={close}>
+            {stringsOverrides?.cancelButton ?? "No"}
+          </Button>
+        </ModalButtonBar>
+      </Modal>
+      {isLoading && <OverlaySpinner />}
+    </>
+  );
+};
+
+const OverlaySpinner = () => {
+  return (
+    <div className="iac-delete-itwin-overlay-container">
+      <ProgressRadial indeterminate />
+    </div>
+  );
+};
