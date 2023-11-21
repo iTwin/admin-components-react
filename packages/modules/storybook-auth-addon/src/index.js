9176c52d183188b3d4d159e437b691204c5050f1
DanishMehmood-bit <64415995+DanishMehmood-bit@users.noreply.github.com>
2023-11-21 20:40:51 +0500

    Updated dependencies to support node 18

--- packages/modules/storybook-auth-addon/src/index.js
+++ packages/modules/storybook-auth-addon/src/index.js
@@ -1,3 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import { BrowserAuthorizationCallbackHandler } from "@bentley/frontend-authorization-client";
 
-BrowserAuthorizationCallbackHandler.handleSigninCallback(window.location.toString()).catch(console.log);
+// eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+BrowserAuthorizationCallbackHandler.handleSigninCallback(
+  window.location.toString()
+).catch(console.log);
