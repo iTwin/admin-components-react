4f8a36ef6bd85f7f6a4e8712783997275e1403d5
Omar H <omar.halbouni@bentley.com>
2024-11-22 11:52:08 -0500

    Add label to favorites button in ITwinTile component

--- packages/modules/imodel-browser/src/containers/ITwinGrid/ITwinTile.tsx
+++ packages/modules/imodel-browser/src/containers/ITwinGrid/ITwinTile.tsx
@@ -98,6 +98,7 @@ export const ITwinTile = ({
         }
         rightIcon={
           <IconButton
+            label={isFavorite ? "Remove from favorites" : "Add to favorites"}
             onClick={async () => {
               isFavorite
                 ? await removeFromFavorites?.(iTwin.id)
