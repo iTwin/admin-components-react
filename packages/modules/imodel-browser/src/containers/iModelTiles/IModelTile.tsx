d86cd8eca04b42b2b5ef2933cb8c1094487326c6
DanishMehmood-bit <64415995+DanishMehmood-bit@users.noreply.github.com>
2025-03-21 21:35:17 +0500

    Fix styles

--- packages/modules/imodel-browser/src/containers/iModelTiles/IModelTile.tsx
+++ packages/modules/imodel-browser/src/containers/iModelTiles/IModelTile.tsx
@@ -2,6 +2,8 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+import "./IModelTile.scss";
+
 import { ThemeProvider, Tile } from "@itwin/itwinui-react";
 import React from "react";
 
