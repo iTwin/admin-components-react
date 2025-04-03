6802a824d7d5801e4d7ed5fca3e4b47cb19afb7e
nimam-bentley <100853841+nimam-bentley@users.noreply.github.com>
2025-04-03 10:42:40 -0400

    removed

--- packages/modules/imodel-browser/src/containers/iModelThumbnail/IModelThumbnail.tsx
+++ packages/modules/imodel-browser/src/containers/iModelThumbnail/IModelThumbnail.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import "./IModelThumbnail.scss";
 
 import { Text, Tile } from "@itwin/itwinui-react";
 import classNames from "classnames";
