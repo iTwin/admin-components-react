75cabb2206d95d13f1d016030bbe5c8daa24b96b
Omar H. <omar.halbouni@bentley.com>
2025-10-24 17:11:15 -0400

    refactor: migrate storybook-auth-addon to Vite
    - Removed .env file and unnecessary public/index.html.
    - Added index.html for the application entry point.
    - Updated package.json to replace react-scripts with Vite for start and build scripts.
    - Introduced new tsconfig.json and tsconfig.node.json for TypeScript configuration.
    - Added vite.config.ts for Vite configuration.
    - Updated dependencies to include Vite and updated TypeScript version.

---
+++ packages/modules/storybook-auth-addon/vite.config.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "build",
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
