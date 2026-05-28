a8d2b03e7a0f6bc85a8aa6896e46eb7ec2b08137
Alex Dunae <alex.dunae@bentley.com>
2026-05-28 08:42:57 -0700

    ThumbnailIconButton

---
+++ packages/modules/imodel-browser/src/components/baseCard/ThumbnailIconButton.tsx
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import IconButton from "@mui/material/IconButton";
+import React from "react";
+
+/**
+ * MUI {@link IconButton} with custom styling to allow it to be placed on top of thumbnail images.
+ *
+ * Used for "favorites" and "more options" on iModel/iTwin tiles and also available to consumers using the thumbnail slots.
+ */
+export function ThumbnailIconButton(
+  props: React.ComponentProps<typeof IconButton>
+) {
+  return (
+    <IconButton
+      {...props}
+      size="small"
+      sx={{
+        bgcolor: "var(--stratakit-color-bg-mono-transparent)",
+        backdropFilter: "blur(0.5rem)",
+        WebkitBackdropFilter: "blur(0.5rem)",
+        mixBlendMode: "exclusion",
+        "&:hover": {
+          bgcolor: "var(--stratakit-color-bg-mono-transparent)",
+        },
+        ...props.sx,
+      }}
+    >
+      {props.children}
+    </IconButton>
+  );
+}
+ThumbnailIconButton.displayName = "ThumbnailIconButton";
