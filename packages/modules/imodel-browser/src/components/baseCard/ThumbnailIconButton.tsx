63bc8e2180ee799634f8339403176360bd73b688
Alex Dunae <alex.dunae@bentley.com>
2026-06-05 11:57:20 -0700

    Remove ThumbnailIconButton styles

--- packages/modules/imodel-browser/src/components/baseCard/ThumbnailIconButton.tsx
+++ packages/modules/imodel-browser/src/components/baseCard/ThumbnailIconButton.tsx
@@ -7,9 +7,6 @@ import type { SxProps, Theme } from "@mui/material/styles";
 import { Icon } from "@stratakit/mui";
 import React from "react";
 
-const spreadSx = (sx: SxProps<Theme> | undefined) =>
-  Array.isArray(sx) ? sx : sx ? [sx] : [];
-
 export interface ThumbnailIconButtonProps
   extends Pick<
     React.ComponentProps<typeof IconButton>,
@@ -43,25 +40,9 @@ export function ThumbnailIconButton(props: ThumbnailIconButtonProps) {
         onClick?.(event);
       }}
       size="small"
-      sx={[
-        (theme) => ({
-          bgcolor: muted ? theme.palette.grey[300] : theme.palette.grey[400],
-          "&:hover": {
-            bgcolor: theme.palette.grey[500],
-          },
-          "&:focus-visible": {
-            bgcolor: theme.palette.grey[500],
-          },
-          "&& .🥝Icon": {
-            "--🥝Icon-color": muted
-              ? theme.palette.grey[50]
-              : theme.palette.common.white,
-          },
-        }),
-        ...spreadSx(sx),
-      ]}
+      sx={sx}
     >
-      <Icon href={icon} size="regular" />
+      <Icon href={icon} />
     </IconButton>
   );
 }
