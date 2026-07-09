583d7200d81b9297247ad6c6460f9fd6508f232b
Omar H. <omar.halbouni@bentley.com>
2026-07-09 13:52:15 -0400

    feat(storybook): add iTwinId argType to IModelGrid and IModelGridMUI components

--- packages/apps/storybook/src/imodel-browser/IModelGrid.stories.tsx
+++ packages/apps/storybook/src/imodel-browser/IModelGrid.stories.tsx
@@ -42,6 +42,9 @@ export default {
   component: IModelGrid,
   argTypes: {
     ...accessTokenArgTypes,
+    iTwinId: {
+      control: { type: "text" },
+    },
     requestType: {
       options: ["all", "recents", "favorites"],
       mapping: {
@@ -285,27 +288,28 @@ const buildMenuItems =
     close: () => void,
     setVersion: React.Dispatch<React.SetStateAction<Version | undefined>>
   ) =>
-  (v: Version) => (
-    <span
-      onClick={(event) => {
-        event.stopPropagation();
-      }}
-    >
-      {v.id === "loading" ? (
-        <MenuItemSkeleton />
-      ) : (
-        <MenuItem
-          key={v.id}
-          onClick={() => {
-            close();
-            v.id !== "loading" && setVersion(v);
-          }}
-        >
-          {v.displayName}
-        </MenuItem>
-      )}
-    </span>
-  );
+  (v: Version) =>
+    (
+      <span
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+      >
+        {v.id === "loading" ? (
+          <MenuItemSkeleton />
+        ) : (
+          <MenuItem
+            key={v.id}
+            onClick={() => {
+              close();
+              v.id !== "loading" && setVersion(v);
+            }}
+          >
+            {v.displayName}
+          </MenuItem>
+        )}
+      </span>
+    );
 
 /** Hook used in StatefulPropsOverrides.args, the function itself must be a stable reference as it is a hook. */
 const useIndividualState = (iModel: IModelFull, props: IModelTileProps) => {
